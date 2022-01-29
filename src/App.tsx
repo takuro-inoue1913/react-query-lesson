664f8662b722e47c55af7f172fecfdfa9dd85fab
takuro-inoue <takuro.inoue@blueish.co.jp>
2022-01-29 14:46:34 +0900

    従来の状態管理実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { VFC } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ClassicalFetchA } from './components/ClassicalFetchA'
+import { ClassicalFetchB } from './components/ClassicalFetchB'
+import { StateProvider } from './context/StateProvider'
+import { Layout } from './components/Layout'
 
-function App() {
+const App: VFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <StateProvider>
+        <Layout>
+          <Routes>
+            <Route path="/fetch-a" element={<ClassicalFetchA />}></Route>
+            <Route path="/fetch-b" element={<ClassicalFetchB />}></Route>
+          </Routes>
+        </Layout>
+      </StateProvider>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ClassicalFetchA.tsx
@@ -0,0 +1,26 @@
+import { VFC } from 'react'
+import { useClassicalFetch } from '../hooks/useClassicalFetch'
+import { useNavigate } from 'react-router-dom'
+import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
+
+export const ClassicalFetchA: VFC = () => {
+  const navigate = useNavigate()
+  const { isLoading, isError, tasks } = useClassicalFetch()
+  console.log('rendered ClassicalFetchA')
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <p className="text-center font-bold mb-3">ClassicalFetchA</p>
+      {isError && <div>Error</div>}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        tasks?.map((task) => <p key={task.id}>{task.title}</p>)
+      )}
+      <ChevronDoubleRightIcon
+        onClick={() => navigate('/fetch-b')}
+        className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
+      />
+      <p className="text-sm">fetch B</p>
+    </div>
+  )
+}
